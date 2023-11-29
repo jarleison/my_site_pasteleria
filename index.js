cf0c3e923cf12432de7413d8eedbdb11824ddabb
jarleison <jarleison.salas.2198@miremington.edu.co>
2023-11-29 03:21:09 -0500

    Modo oscuro

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ document.addEventListener("DOMContentLoaded", function () {
     toggleButton.addEventListener("click", function () {
         cardsContainer.classList.toggle("hidden");
     });
+
+    const toggleButton2 = document.getElementById("toggleButton2");
+    const body = document.body;
+
+    toggleButton2.addEventListener("click", function () {
+        // Cambia entre el modo oscuro y el modo claro
+        body.classList.toggle("dark-mode");
+    });
 });
