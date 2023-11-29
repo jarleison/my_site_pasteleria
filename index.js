4480af8db9f5812f94b55d8563d2671c4ce82ec2
jarleison <jarleison.salas.2198@miremington.edu.co>
2023-11-29 03:47:09 -0500

    funcion numero 3 link a yt

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     const toggleButton = document.getElementById("toggleButton");
     const cardsContainer = document.querySelector(".cards-container");
+    const openYouTubeButton = document.getElementById("openYT");
+    const toggleButton2 = document.getElementById("toggleButton2");
+    const body = document.body;
 
     toggleButton.addEventListener("click", function () {
         cardsContainer.classList.toggle("hidden");
     });
 
-    const toggleButton2 = document.getElementById("toggleButton2");
-    const body = document.body;
-
     toggleButton2.addEventListener("click", function () {
         // Cambia entre el modo oscuro y el modo claro
         body.classList.toggle("dark-mode");
     });
+
+    openYouTubeButton.addEventListener("click", function () {
+        // Abre YouTube en una nueva ventana o pestaña
+        window.open("https://www.youtube.com", "_blank");
+});
 });
